6b0bc80e2360f2878599542605e1d14cd60803c2
DaGa123 <DaGa123>
2023-01-27 18:48:29 +0100

    structural preparation : components / services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoreComponent } from './store/store.component';
+import { PaymentComponent } from './payment/payment.component';
+import { VaultComponent } from './vault/vault.component';
+import { FooterComponent } from './footer/footer.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { ProductComponent } from './store/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoreComponent,
+    PaymentComponent,
+    VaultComponent,
+    FooterComponent,
+    ButtonComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/vault/vault.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vault',
+  templateUrl: './vault.component.html',
+  styleUrls: ['./vault.component.less']
+})
+export class VaultComponent {
+
+}
